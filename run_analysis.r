f1513318ceeb15960d7565a7387aa4567a70d304
Mohammed Ali <moh_fcis@yahoo.com>
2016-04-15 18:51:11 +0200

    Clean Company column

---
+++ run_analysis.r
@@ -0,0 +1,17 @@
+suppressMessages(library(dplyr))
+suppressMessages(library(tidyr))
+#Load Data
+original_refine = tbl_df(read.csv("data/refine_original.csv", stringsAsFactors = FALSE))
+######################################
+# 1- Clean Company column
+######################################
+original_refine$company = tolower(original_refine$company)
+##Correct 'philips'
+original_refine$company = gsub('(^[p].*)', x = original_refine$company, replacement = 'philips')
+original_refine$company = gsub('(^[f].*)', x = original_refine$company, replacement = 'philips')
+
+##Correct 'akzo'
+original_refine$company = gsub('(^[ak].*)', x = original_refine$company, replacement = 'akzo')
+
+##Correct 'unilever'
+original_refine$company = gsub('(^[un].*)', x = original_refine$company, replacement = 'unilever')
